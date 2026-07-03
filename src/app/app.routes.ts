70b791661ffa599aebe759d7a5da7ba2f968d652
Bhavesh Shah <>
2026-07-04 00:52:10 +0200

    push code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
--- src/app/practices/forms/reactive-forms/reactive-forms.ts
+++ src/app/practices/forms/reactive-forms/reactive-forms.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-forms.html',
   styleUrl: './reactive-forms.scss',
 })
 export class ReactiveForms {
-
 }
